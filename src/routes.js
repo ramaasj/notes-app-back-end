6ab4fa3e357753e8abf2df1e2ebe8c0e6ead6439
Rama <ramajanitra999@gmail.com>
2023-03-24 14:55:26 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const { addNoteHandler, getAllNotesHandler } = require('./handler');
+
+/* eslint-disable linebreak-style */
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+];
+
+module.exports = routes;
